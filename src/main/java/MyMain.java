23e0c42aa7bcf1ed0a841f4a12556b6b7ca89219
ethanarnold <emailtoethan@gmail.com>
2021-09-13 12:12:06 -0700

    did stuff with mod

--- src/main/java/MyMain.java
+++ src/main/java/MyMain.java
@@ -4,8 +4,7 @@ public class MyMain {
     // Ex. divisibleByFour(16) -> true
     //     divisibleByFour(5) -> false
     public static boolean divisibleByFour(int num) {
-        // REPLACE WITH YOUR CODE HERE
-        return false;
+        return (num%4==0);
     }
 
     // This method prints every number from 1 up to and including the input biggest
@@ -14,15 +13,25 @@ public class MyMain {
     // 6 7 8 9 10
     // ...
     public static void printNumsUpTo(int biggest) {
-        // REPLACE THIS WITH YOUR CODE
+        int i = 1;
+        while(i<=biggest) {
+            if(i%5==0) System.out.println(i + " ");
+            else System.out.print(i + " ");
+            i++;
+        }
     }
 
     // This method is given an input x, and it adds up the sum of its digits
     // Ex. sumDigits(61) -> 7 because 6 + 1 = 7
     //     sumDigits(123) -> 6 because 1 + 2 + 3 = 6
     public static int sumDigits(int x) {
-        // REPLACE THIS WITH YOUR CODE
-        return 0;
+        int num = x;
+        int res = 0;
+        while(num>0) {
+            res += num % 10; //add last digit to result
+            num /= 10; //cut off last digit
+        }
+        return res;
     }
 
     // Bonus: This method returns true if the input num is a "self-divisor",
@@ -35,8 +44,14 @@ public class MyMain {
     // Note that 0 is not considered to be a divisor of any number, so any number containing a 0
     // digit is NOT a self-divisor.
     public static boolean isSelfDivisor(int num) {
-        // REPLACE THIS WITH YOUR CODE
-        return false;
+        if(String.valueOf(num).contains("0")) return false; //if contains 0 return false
+        int x = num; //x is just the input but mutable. honestly don't know if this is necessary
+        int res = 0; //result var
+        while(x>0) {
+            if(!(num % (x % 10) == 0)) return false; //test for divisibility by last digit
+            x /= 10; //cut off last digit
+        }
+        return true;
     }
 
     public static void main(String[] args) {
